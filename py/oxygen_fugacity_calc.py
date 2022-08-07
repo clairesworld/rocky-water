8462519affa7d198f6e7b2534067a2d225914842
claire <claire.guimond@gmail.com>
2022-08-07 19:09:02 +0100

    running on apollo

--- py/oxygen_fugacity_calc.py
+++ py/oxygen_fugacity_calc.py
@@ -8,7 +8,9 @@ T_min, T_max = 1372.5, 1900.5  # endpoint can't equal T_of_interest
 
 X_ferric = 0.03
 core_eff = 0.88
-output_parent_path = pf.output_parent_apollo + 'hypatia' + str(int(core_eff * 100)) + 'Fe/'
+output_sub = 'hypatia' + str(int(core_eff * 100)) + 'Fe/'
+output_parent_path = pf.output_parent_apollo + output_sub
+
 
 # # only need to run this once to get build files (i.e., bulk composition) and vertex output files
 # note this will first download the stellar composisions for the whole sample and then run perple_x
@@ -16,7 +18,7 @@ pf.fo2_from_hypatia(p_min, p_max, n_sample=-1, T_min=T_min, T_max=T_max, isother
                  X_ferric=X_ferric, core_efficiency=core_eff, planet_kwargs={'Tp': 999},
                  check_comp=True, suppress_output=False, run=True, verbose=True, output_parent_path=output_parent_path,
                  mu0_file='data_tables/mu_o2_standard.tab', compare_buffer='qfm',
-                 use_local_composition=True, existing_dir=output_parent_path,  # try local first
+                 use_local_composition=True, existing_dir=output_sub,  # try local first
                  )
 
 # # can (re)do fo2 calculations with this using existing vertex files
