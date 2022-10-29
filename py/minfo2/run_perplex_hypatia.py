416eea513f16430f5962a7f0332a9728568d847c
claire <claire.guimond@gmail.com>
2022-10-29 18:32:06 +0100

    running on apollo

--- py/minfo2/run_perplex_hypatia.py
+++ py/minfo2/run_perplex_hypatia.py
@@ -38,7 +38,7 @@ pf.fo2_from_hypatia(p_min, p_max, n_sample=-1, T_min=T_min, T_max=T_max, isother
                     check_comp=True, suppress_output=False, run=True, verbose=True,
                     output_parent_path=output_parent_path, perplex_path=perplex_path,
                     mu0_file='data_tables/mu_o2_standard.tab', compare_buffer='qfm',
-                    use_local_compositon=False, names_file='/home/claire/Works/rocky-water/py/host_names.txt',
+                    use_local_compositon=False, names_file='/home/cmg76/Works/rocky-water/py/host_names.txt',
                     # use_local_composition=True, existing_dir='hypatia88Fe/',  # try local first
                     # existing_output_parent=pf.output_parent_apollo,  <== existing kwarg
                     # restart='2MASS 23155829+3127462'
