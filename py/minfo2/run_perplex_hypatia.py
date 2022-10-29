885cbdba7765985e4dae4f1ae2a2c1df4530923c
claire <claire.guimond@gmail.com>
2022-10-29 18:25:13 +0100

    running on apollo

--- py/minfo2/run_perplex_hypatia.py
+++ py/minfo2/run_perplex_hypatia.py
@@ -4,6 +4,7 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 print('SCRIPT_DIR', SCRIPT_DIR)
+
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import perplexfugacitydata as pf
