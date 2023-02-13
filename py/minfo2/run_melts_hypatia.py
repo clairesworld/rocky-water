3ac2d76f0b9d408649d83a3b78a167b73735c66e
claire <claire.guimond@gmail.com>
2023-02-13 15:08:38 +0000

    running on apollo

--- py/minfo2/run_melts_hypatia.py
+++ py/minfo2/run_melts_hypatia.py
@@ -58,7 +58,8 @@ for ce in core_eff:
                             perplex_path=perplex_path,  # for qfm data table
                             names_file=names_file,
                             # use_local_composition=False,
-                            use_local_composition=True, existing_dir='hypatia_88coreeff_3ferric_ext/',  # try local first
+                            # use_local_composition=True, existing_dir='hypatia_88coreeff_3ferric_ext/',  # try local first
+                            use_local_composition=True, existing_dir=output_parent_path,
                             existing_output_parent=source,  # '/raid1/cmg76/perple_x/output/rocky-fo2/',
                             suffix=str(Xf*100).replace('.', ',') + 'fer',
                             skip_names=skip_stars,  # []
