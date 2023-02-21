bcae9087d76697f960d5e1e8e2b963a17f4aaf81
claire <claire.guimond@gmail.com>
2023-02-21 10:41:36 +0000

    running on apollo

--- py/minfo2/process_melts_hypatia.py
+++ py/minfo2/process_melts_hypatia.py
@@ -57,7 +57,7 @@ for ce in core_eff:
         mfug.fo2_from_local(output_parent_path, core_efficiency=ce, X_ferric=Xf, alphamelts_path=alphamelts_path,
                             compare_buffer='qfm', perplex_path=perplex_path, T_of_interest=T_of_interest, save=True,
                             verbose=False,
-                            restart='2MASS19141179+3833548'
+                            # restart='2MASS19141179+3833548'
                             )
 
 """ ^^^ UNCOMMENT TO RUN ^^^ """
