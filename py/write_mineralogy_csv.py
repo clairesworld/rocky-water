a7e0a648c534cbc872ec0655d129ffe83bfc1b92
clairesworld <claire.guimond@gmail.com>
2023-10-31 14:33:32 +0000

    table

--- py/write_mineralogy_csv.py
+++ py/write_mineralogy_csv.py
@@ -5,7 +5,8 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(SCRIPT_DIR)
-sys.path.append(os.path.dirname(PARENT_DIR))
+# sys.path.append(os.path.dirname(PARENT_DIR))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 import parameters as p
 import main as rw
