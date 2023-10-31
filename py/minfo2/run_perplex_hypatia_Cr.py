112f4bfb1e96b7576b35f0ca13308321bb1c25b8
clairesworld <claire.guimond@gmail.com>
2023-10-31 14:40:11 +0000

    table

--- py/minfo2/run_perplex_hypatia_Cr.py
+++ py/minfo2/run_perplex_hypatia_Cr.py
@@ -2,9 +2,9 @@ import numpy as np
 import sys
 import os
 
-# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-# PARENT_DIR = os.path.dirname(SCRIPT_DIR)
-# sys.path.append(os.path.dirname(PARENT_DIR))
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(SCRIPT_DIR)
+sys.path.append(os.path.dirname(PARENT_DIR))
 # print('sys.path', sys.path)
 
 import perplexfugacitydata as pf
