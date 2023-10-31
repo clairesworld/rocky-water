05a58f15f3987e8bef19f475d52044b6136b8a28
clairesworld <claire.guimond@gmail.com>
2023-10-31 14:06:18 +0000

    table

--- py/write_mineralogy_csv.py
+++ py/write_mineralogy_csv.py
@@ -1,5 +1,11 @@
 import numpy as np
 import pandas as pd
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(SCRIPT_DIR)
+sys.path.append(os.path.dirname(PARENT_DIR))
+
 import parameters as p
 import main as rw
 import perplexdata as px
